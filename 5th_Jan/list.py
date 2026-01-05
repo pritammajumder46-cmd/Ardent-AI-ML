2ecba2321ea2f3bde799d5b6650d9836f3b42782
Pritam Majumder <pritammajumder46@gmail.com>
2026-01-05 22:18:14 +0530

    Class conducted by Tridip Kundu sir(Ardent)

---
+++ 5th_Jan/list.py
@@ -0,0 +1,17 @@
+# Listproperties
+
+# 1. Ordered 2. Changebale 3. Duplication is Allowed
+
+#Write a programme to create a list and that contains 5 elements
+
+list_s = [1,2,3,4,5]
+print(list_s)
+print(type(list_s))
+
+#Printa even number from a list
+
+l=[1,2,3,4,5,6,7,8,9,10]
+
+for i in l:
+    if i%2 == 0:
+        print(i)
