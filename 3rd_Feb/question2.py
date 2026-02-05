a9b81cd8ab7cbe75815037a9e9780bb7abf6b1e6
Pritam Majumder <pritammajumder46@gmail.com>
2026-02-05 11:56:10 +0530

    Lines problem fixed

--- 3rd_Feb/question2.py
+++ 3rd_Feb/question2.py
@@ -11,17 +11,23 @@ except FileExistsError:
 
 
 with open("Myself.txt", "w") as file:
-    file.write("Hi, I am Pritam. I am a good boy who loves learning and coding. I like to play guitar, study hard, and dream big for my future. I am from West Bengal and I speak Bengali and English. I want to go to IIT and become a smart engineer. I am friendly, funny, and I like to make people smile. I always try my best like a cute baby student")
+    file.write("Hi, I am Pritam. I am a good boy who loves learning and coding. I like to play guitar, study hard, and dream big for my future. I am from West Bengal and I speak Bengali and English. I want to go to IIT and become a smart engineer. I am friendly, funny, and I like to make people smile. I always try my best like a cute baby student\nHello Everyone")
 
 with open("MySelf.txt", "r") as file:
     paragraph = file.read()
     print(paragraph)
 
 
-lines = 0
+# lines = 0
 
 with open("MySelf.txt", "r") as file:
-    for check_lines in paragraph:
-        lines += 1 
+    lines = len(file.readlines())
+
+# print(f"\n{lines}")
 
 print(f"\nTotal number of lines in the paragraph is {lines}\n")
+
+#with open("filename.txt", "r") as file:
+    # lines = len(file.readlines())
+
+# print(lines)
